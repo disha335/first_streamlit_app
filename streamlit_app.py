7ba31e3e6a1daf86a57e25bc26bf73995963fab5
Disha Handa <disha55handa@gmail.com>
2023-08-10 00:42:22 +0530

    added some items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's new healthy diner')
+streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🥣Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗Kale , spinach & rocket smoothie')

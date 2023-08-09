23f2da4877550db2af29f84ddf1d160330e9526b
Disha Handa <disha55handa@gmail.com>
2023-08-10 00:38:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale , spinach & rocket smoothie')
+streamlit.text('Hand-bolied Free-range Egg')

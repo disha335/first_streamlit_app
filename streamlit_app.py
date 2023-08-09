e723c45e3bcc44d2fac0b22d30633449e846f55b
Disha Handa <disha55handa@gmail.com>
2023-08-10 00:47:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import pandas
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🥣Omega 3 & Blueberry oatmeal')
@@ -7,5 +6,6 @@ streamlit.text('🥗Kale , spinach & rocket smoothie')
 streamlit.text('🐔Hand-bolied Free-range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list).

0f7b31c2e0e2ee75d753b5c717fbd6b827317ddf
Disha Handa <disha55handa@gmail.com>
2023-08-13 00:25:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,12 @@ streamlit.text('🥗Kale , spinach & rocket smoothie')
 streamlit.text('🐔Hand-bolied Free-range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.header('Fruityvice Fruit Advice!')
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),default=['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
